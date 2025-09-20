093b49e575c079ae5be7940215c13159a8347dd7
Salvado_Michael <salvadmichael24@gmail.com>
2025-09-20 21:15:28 +0800

    CommentSection

--- CommentSection.js
+++ CommentSection.js
@@ -4,7 +4,7 @@ import { View, Text, FlatList, TextInput, TouchableOpacity, StyleSheet } from "r
 export default function CommentSection() {
   const [comments, setComments] = useState([
     { id: "1", text: "Maayong gabie" },
-    { id: "2", text: "Wow palo" },
+    { id: "2", text: "Wow " },
   ]);
   const [newComment, setNewComment] = useState("");
 
